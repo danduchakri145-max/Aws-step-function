670238dda983d04da6102bd8ed5492e8e24a7faa
danduchakri145-max <danduchakri145@gmail.com>
2026-03-05 17:56:05 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,16 +1,37 @@
-resource "aws_iam_role" "step_role" {
-name = "abc-step-function-role-v2"
-
-assume_role_policy = jsonencode({
-Version = "2012-10-17"
-Statement = [
-{
-Effect = "Allow"
-Principal = {
-Service = "states.amazonaws.com"
+ resource "aws_iam_role" "step_role" {
+  name = "abc-step-function-role-v2"
+ 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "states.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
-Action = "sts:AssumeRole"
+ 
+resource "aws_iam_role_policy" "step_policy" {
+  name = "step-function-policy"
+  role = aws_iam_role.step_role.id
+ 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "events:PutRule",
+          "events:PutTargets",
+          "events:DescribeRule"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
-]
-})
-}
+ 
