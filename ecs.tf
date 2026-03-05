ad5cba2eeafbb1af2dbc867fa4d970f3ac9ff610
EC2 Default User <ec2-user@ip-172-31-19-187.us-west-2.compute.internal>
2026-03-05 10:02:37 +0000

    initial commit

---
+++ ecs.tf
@@ -0,0 +1,26 @@
+resource "aws_ecs_cluster" "main" {
+  name = "abc-cluster"
+}
+resource "aws_ecs_task_definition" "task" {
+  family                   = "abc-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+ 
+container_definitions = jsonencode([
+  {
+    name      = "abc-container"
+    image     = "amazonlinux"
+    cpu       = 256
+    memory    = 512
+    essential = true
+ 
+    command = [
+      "sh",
+      "-c",
+      "echo ECS task completed; sleep 5"
+    ]
+  }
+])
+} 
---
+++ stepfunction.tf
@@ -0,0 +1,46 @@
+resource "aws_sfn_state_machine" "workflow" {
+name = "abc-workflow"
+role_arn = aws_iam_role.step_role.arn
+
+definition = jsonencode({
+Comment = "ABC Startup Transaction Processing Workflow"
+StartAt = "CheckEC2"
+
+States = {
+
+  CheckEC2 = {
+    Type = "Pass"
+    Result = "EC2 instance validated"
+    Next = "RunECSTask"
+  }
+
+  RunECSTask = {
+    Type = "Task"
+    Resource = "arn:aws:states:::ecs:runTask.sync"
+
+    Parameters = {
+      LaunchType = "FARGATE"
+
+      Cluster = aws_ecs_cluster.main.id
+
+      TaskDefinition = aws_ecs_task_definition.task.arn
+NetworkConfiguration = {
+AwsvpcConfiguration = {
+Subnets = [
+aws_subnet.public.id
+]
+
+SecurityGroups = [
+  aws_security_group.ec2_sg.id
+]
+AssignPublicIp = "ENABLED"
+}
+} }
+
+    End = true
+  }
+
+}
+})
+}
+
